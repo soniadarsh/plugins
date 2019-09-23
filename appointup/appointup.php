db2c80b03382d65f3adb6996df3dcb0cdd1bf48c
Adarsh Soni <adarshsoni500@gmail.com>
2019-09-23 16:57:18 +0530

    Updated appointUp version-1.0.4

--- appointup/appointup.php
+++ appointup/appointup.php
@@ -390,7 +390,6 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	__FILE__,
 	'appointup'
 );
-
- $myUpdateChecker->setAuthentication('ae88873ac6f45c47aaf572cc9f7e00ceb06c30b5');
-$myUpdateChecker->setBranch('master');
+//$myUpdateChecker->setAuthentication('ae88873ac6f45c47aaf572cc9f7e00ceb06c30b5');
+    $myUpdateChecker->setBranch('master');
 $myUpdateChecker->getVcsApi()->enableReleaseAssets();
